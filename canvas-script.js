badeafe51f03637fcb06174b2fdf907c9d96cdbb
Gabriella Cannon <gabriellakate04@gmail.com>
2020-06-14 18:53:13 +0200

    rectangles

---
+++ canvas-script.js
@@ -0,0 +1,11 @@
+// Definitions
+const canvas = document.getElementById("hello-world-canvas");
+const context = canvas.getContext("2d");
+
+// Blue Rectangle
+context.fillStyle = "blue";
+context.fillRect(10, 40, 30, 70);
+
+// Yellow Rectangle
+context.fillStyle = "yellow";
+context.fillRect(50, 30, 60, 30);
